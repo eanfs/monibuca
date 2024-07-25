c66111a25260d3ca3568024fc6647195809ac837
richard.li <lirichen32@gmail.com>
2024-07-25 18:12:57 +0800

    Update for common build

--- build_release.sh
+++ build_release.sh
@@ -7,7 +7,7 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o monibuca-linux-amd64
 echo "Building for Linux ARM64..."
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o monibuca-linux-arm64
 
-bash -x build_image.sh 4.7.4.SP5
+bash -x build_image.sh 4.7.5
 
 
 rm -rf monibuca-linux-amd64
