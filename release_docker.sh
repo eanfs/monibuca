478424eee8336ea01c61a7d59e56c9ef0f7dbdd8
richard.li <lirichen32@gmail.com>
2024-09-25 11:26:08 +0800

    Update to v4.8.1

--- release_docker.sh
+++ release_docker.sh
@@ -7,7 +7,7 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o monibuca-linux-amd64
 echo "Building for Linux ARM64..."
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o monibuca-linux-arm64
 
-bash -x build_image.sh 4.8.0
+bash -x build_image.sh 4.8.1
 
 
 rm -rf monibuca-linux-amd64
